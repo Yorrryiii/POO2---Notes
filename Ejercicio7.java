820acdd572de765deea2d14c2c11de95162ee723
Mario Gómez <alu.124956@usj.es>
2023-02-10 14:30:35 +0100

    Intentando solucionar el ejercicio 7

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -24,12 +24,7 @@ public class Ejercicio7 {
                 }
                 break;
             case "derecha":
-                // for (int i = 1; i <= height; i++) {
-                //     for (int j = 1; j <= i; j++) {
-                //         System.out.print("* ");
-                //     }
-                //     System.out.println();
-                // }
+
                 break;
             case "abajo":
                 for (int i = height; i >= 1; i--) {
